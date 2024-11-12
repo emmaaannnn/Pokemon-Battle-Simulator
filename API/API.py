8de7347f01f802e288cedb29d28546719b0849a1
Reece <r.tredwell@outlook.com>
2024-11-12 12:02:31 +1100

    fixed the main class so that it now works a it better in the sense of returning the selected team and opp - Also changed so that the file name for each pokemon is the name rather than the id, this way we can use the string in the main function to open the json file

--- API/API.py
+++ API/API.py
@@ -2,48 +2,52 @@ import requests
 from pprint import pprint
 import json
 
-# for i in range(1, 152):
-#     url = f"https://pokeapi.co/api/v2/pokemon/{i}/"
-#     response = requests.get(url)
-#     data = response.json()
-#     # simplified_data = {
-#     #     "name": data["name"],
-#     #     "id": data["id"],
-#     #     "types": [type_info["type"]["name"] for type_info in data["types"]],
-#     #     "base_stats": {stat["stat"]["name"]: stat["base_stat"] for stat in data["stats"]}
-#     # }
-#     # pprint(simplified_data)
-
-#     with open("Pokemon/" + str(i) +".json", 'w') as f:
-#         json.dump(data,f)
 
 def main():
+    print("starting fetch")
     for i in range(1, 152):
-        moves = []
-        with open("Pokemon/" + str(i) +".json", 'r') as file:
-            data = json.load(file)
-            for item in data["moves"]:
-                name = item["move"]["name"]
-                url = item["move"]["url"]   
-                response = requests.get(url)
-                data = response.json()
-                simplified_data = {
-                    "id": data["id"],
-                    "name": data["name"],
-                    "accuracy": data["accuracy"],
-                    "pp": data["pp"],
-                    "priority": data["priority"],
-                    "power": data["power"],
-                    "damage_class": data["damage_class"],
-                    "effect": data["effect_entries"],
-                    "info":{
-                        "ailment": data["meta"]
-                    }
-                }
-                pprint(simplified_data)
-                moves.append(simplified_data)
+        url = f"https://pokeapi.co/api/v2/pokemon/{i}/"
+        response = requests.get(url)
+        data = response.json()
+
+        # simplified_data = {
+        #     "name": data["name"],
+        #     "id": data["id"],
+        #     "types": [type_info["type"]["name"] for type_info in data["types"]],
+        #     "base_stats": {stat["stat"]["name"]: stat["base_stat"] for stat in data["stats"]}
+        # }
+        # pprint(simplified_data)
+
+        with open("Pokemon/" + data["forms"][0]["name"] +".json", 'w') as f:
+            json.dump(data,f)
+
+# def main():
+#     for i in range(1, 152):
+#         moves = []
+#         with open("Pokemon/" + str(i) +".json", 'r') as file:
+#             data = json.load(file)
+#             for item in data["moves"]:
+#                 name = item["move"]["name"]
+#                 url = item["move"]["url"]   
+#                 response = requests.get(url)
+#                 data = response.json()
+#                 simplified_data = {
+#                     "id": data["id"],
+#                     "name": data["name"],
+#                     "accuracy": data["accuracy"],
+#                     "pp": data["pp"],
+#                     "priority": data["priority"],
+#                     "power": data["power"],
+#                     "damage_class": data["damage_class"],
+#                     "effect": data["effect_entries"],
+#                     "info":{
+#                         "ailment": data["meta"]
+#                     }
+#                 }
+#                 pprint(simplified_data)
+#                 moves.append(simplified_data)
   
-        with open("moves/" + str(i) +".json", 'w') as file:
-            data = json.dump(moves, file)
+#         with open("moves/" + str(i) +".json", 'w') as file:
+#             data = json.dump(moves, file)
 
 main()
