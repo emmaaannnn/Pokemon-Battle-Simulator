a6ee60132e5b5c1c31a38a97112846845a948676
Emmanuel Almonte <emmanuel.almonte@students.mq.edu.au>
2024-11-14 14:09:45 +1100

    Created a Team.cpp file - moves included with team

--- main.cpp
+++ main.cpp
@@ -29,13 +29,20 @@ std::unordered_map<string, vector<string>> selectTeamAndOpponent() {
     // Print available teams
     unordered_map<int, vector<string>> Available_teams;
 
-    Available_teams[1] = {"Clefable", "Gengar", "Kangaskhan", "Victreebel", "Ninetales", "Blastoise"};
-    Available_teams[2] = {"Pikachu", "Machamp", "Arcanine", "Lapras", "Snorlax", "Venusaur"};
-    Available_teams[3] = {"Tauros", "Gyarados", "Aerodactyl", "Alakazam", "Exeggutor", "Charizard"};
+    Available_teams[1] = {"Venusaur"};
+    Available_teams[2] = {"Charizard"};
+    Available_teams[3] = {"Blastoise"};
+
+    // Available_teams[1] = {"Clefable", "Gengar", "Kangaskhan", "Victreebel", "Ninetales", "Blastoise"};
+    // Available_teams[2] = {"Pikachu", "Machamp", "Arcanine", "Lapras", "Snorlax", "Venusaur"};
+    // Available_teams[3] = {"Tauros", "Gyarados", "Aerodactyl", "Alakazam", "Exeggutor", "Charizard"};
     cout << "Available teams:" << endl;
-    cout << "[1] - Clefable, Gengar, Kangaskhan, Victreebel, Ninetales, Blastoise" << endl;
-    cout << "[2] - Pikachu, Machamp, Arcanine, Lapras, Snorlax, Venusaur" << endl;
-    cout << "[3] - Tauros, Gyarados, Aerodactyl, Alakazam, Exeggutor, Charizard" << endl;
+    cout << "[1] - Venusaur" << endl;
+    cout << "[2] - Charizard" << endl;
+    cout << "[3] - Blastoise" << endl;
+    // cout << "[1] - Clefable, Gengar, Kangaskhan, Victreebel, Ninetales, Blastoise" << endl;
+    // cout << "[2] - Pikachu, Machamp, Arcanine, Lapras, Snorlax, Venusaur" << endl;
+    // cout << "[3] - Tauros, Gyarados, Aerodactyl, Alakazam, Exeggutor, Charizard" << endl;
     cout << "" << endl;
 
 
@@ -58,23 +65,30 @@ std::unordered_map<string, vector<string>> selectTeamAndOpponent() {
 
     // Prompt user to select opponent
     unordered_map<int,vector<string>> opponent;
-    opponent[1] = {"Gym Leader Brock"};
-    opponent[2] = {"Gym Leader Misty"};
-    opponent[3] = {"Gym Leader Lt. Surge"};
-    opponent[4] = {"Gym Leader Erika"};
-    opponent[5] = {"Gym Leader Koga"};
-    opponent[6] = {"Gym Leader Sabrina"};
-    opponent[7] = {"Gym Leader Blaine"};
-    opponent[8] = {"Gym Leader Giovanni"};
+    opponent[1] = {"Venasaur"};
+    opponent[1] = {"Charizard"};
+    opponent[1] = {"Blastoise"};
+    // opponent[1] = {"Gym Leader Brock"};
+    // opponent[2] = {"Gym Leader Misty"};
+    // opponent[3] = {"Gym Leader Lt. Surge"};
+    // opponent[4] = {"Gym Leader Erika"};
+    // opponent[5] = {"Gym Leader Koga"};
+    // opponent[6] = {"Gym Leader Sabrina"};
+    // opponent[7] = {"Gym Leader Blaine"};
+    // opponent[8] = {"Gym Leader Giovanni"};
+
     cout << "Select your opponent:" << endl;
-    cout << "[1] - Gym Leader Brock" << endl;
-    cout << "[2] - Gym Leader Misty" << endl;
-    cout << "[3] - Gym Leader Lt. Surge" << endl;
-    cout << "[4] - Gym Leader Erika" << endl;
-    cout << "[5] - Gym Leader Koga" << endl;
-    cout << "[6] - Gym Leader Sabrina" << endl;
-    cout << "[7] - Gym Leader Blaine" << endl;
-    cout << "[8] - Gym Leader Giovanni" << endl;
+    cout << "[1] - Venasaur" << endl;
+    cout << "[2] - Charizard" << endl;
+    cout << "[3] - Blastoise" << endl;
+    // cout << "[1] - Gym Leader Brock" << endl;
+    // cout << "[2] - Gym Leader Misty" << endl;
+    // cout << "[3] - Gym Leader Lt. Surge" << endl;
+    // cout << "[4] - Gym Leader Erika" << endl;
+    // cout << "[5] - Gym Leader Koga" << endl;
+    // cout << "[6] - Gym Leader Sabrina" << endl;
+    // cout << "[7] - Gym Leader Blaine" << endl;
+    // cout << "[8] - Gym Leader Giovanni" << endl;
 
     int chosenOpponentNum;
     cout << "Enter your chosen opponent: ";
