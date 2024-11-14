99c08f8df926bd3a2f54d4e255d593b05f1f6705
Emmanuel Almonte <emmanuel.almonte@students.mq.edu.au>
2024-11-14 16:26:03 +1100

    Team.cpp still a work in progress

--- Move.cpp
+++ Move.cpp
@@ -16,8 +16,6 @@ class Move{
         
         //type of move
         std::string damage_class;
-        std::string effect;
-        std::string short_effect;
         
         //move effects
         int ailment_chance;
@@ -32,4 +30,30 @@ class Move{
         int min_hits;
         int min_turns;
         int stat_chance;
+
+    Move(const json& move) {
+        // Basic move attributes
+        name = move["name"];
+        accuracy = move["accuracy"];
+        effect_chance = move["effect_chance"];
+        pp = move["pp"];
+        priority = move["priority"];
+        power = move["power"];
+        
+        // Damage class (extract only the name field)
+        damage_class = move["damage_class"]["name"];
+
+        // Info section (ailment and category)
+        ailment_chance = move["Info"]["ailment_chance"];
+        category = move["Info"]["category"]["name"];
+        crit_rate = move["Info"]["crit_rate"];
+        drain = move["Info"]["drain"];
+        flinch_chance = move["Info"]["flinch_chance"];
+        healing = move["Info"]["healing"];
+        max_hits = move["Info"]["max_hits"];
+        max_turns = move["Info"]["max_turns"];
+        min_hits = move["Info"]["min_hits"];
+        min_turns = move["Info"]["min_turns"];
+        stat_chance = move["Info"]["stat_chance"];
+    }
 };
